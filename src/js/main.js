c9bcb1f1a01af3657c350dca4e67d9dca5017e72
ApeIsHere <vladimir.volkov.mg@gmail.com>
2023-08-29 18:00:03 +0300

    Side Nav Tweek, animated articles v1
    Fixed the issue with side nav classes when 'click' event happens,
    Added animation to articles to show up when we scroll down the page

--- src/js/main.js
+++ src/js/main.js
@@ -8,7 +8,9 @@ document.addEventListener('DOMContentLoaded', () => {
         sections = document.querySelectorAll('[data-section]'),
         positionsTop = [],
         positionsBot = [];
-    let nextLogIndex = 0;
+    let nextLogIndex = 0,
+        isChanged = false,
+        isClicked = false;
 
     sections.forEach((section, i) => {
         positionsTop[i] = section.getBoundingClientRect().top;
@@ -20,50 +22,79 @@ document.addEventListener('DOMContentLoaded', () => {
     //--------------------------------------------   Side-nav-animation
     function changeActiveNavItem(index) {
         const items = document.querySelectorAll('.sidenav__item');
-        console.log(items);
+
 
         items.forEach(item => {
             item.addEventListener('click', () => {
                 items.forEach(item => {
+                    isClicked = true;
                     item.classList.remove('active');
                 });
                 item.classList.add('active');
-            });
-        });
 
-        if (index || index === 0) {
-            items[index].click();
-        }
+                setTimeout(() => {
+                    isClicked = false;
+                }, 800);
+            });
+
+            if (!isClicked && index >= 0) {
+                item.classList.remove('active');
+                items[index].classList.add('active');
+            }
+        });    
     }
 
     changeActiveNavItem();
 
-    console.log(positionsTop);
-    console.log(positionsBot);
-
     parallaxContainer.addEventListener('scroll', () => {
-        const offset = parallaxContainer.scrollTop;
-        const middleOfViewport = window.innerHeight / 2; // Middle of the viewport
-
-        // Check if the top of the section reaches the middle of the viewport when scrolling down
-        if (nextLogIndex < positionsTop.length - 1 && offset + middleOfViewport >= positionsTop[nextLogIndex + 1]) {
-            nextLogIndex++;
-            changeActiveNavItem(nextLogIndex);
-            console.log('Down');
-            console.log(`Position: ${positionsTop[nextLogIndex]}, Index: ${nextLogIndex}, Offset: ${offset}`);
-            
-        }
-
-        // Check if the bottom of the section reaches the middle of the viewport when scrolling up
-        if (nextLogIndex > 0 && offset + middleOfViewport <= positionsBot[nextLogIndex - 1]) {
-            nextLogIndex--;
-            changeActiveNavItem(nextLogIndex);
-            console.log('Up');
-            console.log(`Position: ${positionsBot[nextLogIndex]}, Index: ${nextLogIndex}, Offset: ${offset}`);
+        if(!isClicked) {
+            const offset = parallaxContainer.scrollTop,
+                  botOfViewport = window.innerHeight * .75,
+                  topOfViewport = window.innerHeight * .25;
+    
+            // Check if the top of the section reaches the middle of the viewport when scrolling down
+            if (nextLogIndex < positionsTop.length - 1 && offset + botOfViewport >= positionsTop[nextLogIndex + 1]) {
+                nextLogIndex++;
+                changeActiveNavItem(nextLogIndex);
+                animateArticles(nextLogIndex);
+                console.log('Down');
+                console.log(`Position: ${positionsTop[nextLogIndex]}, NextIndex: ${nextLogIndex}, Offset: ${offset}`);
+                
+            }
+    
+            // Check if the bottom of the section reaches the middle of the viewport when scrolling up
+            if (nextLogIndex > 0 && offset + topOfViewport <= positionsBot[nextLogIndex - 1]) {
+                nextLogIndex--;
+                changeActiveNavItem(nextLogIndex);
+                animateArticles(nextLogIndex);
+                console.log('Up');
+                console.log(`Position: ${positionsBot[nextLogIndex]}, NextIndex: ${nextLogIndex}, Offset: ${offset}`);
+            }
         }
     });
 
+    //--------------------------------------------   Slide the blocks in and out
 
+    const articleItems = document.querySelectorAll('.article__text, .article__img');
+
+    articleItems.forEach((item, i) => {
+        item.style.display = 'none';
+        item.classList.add('animate__animated');
+        
+        if(i % 2 == 0) {
+            item.classList.add('animate__fadeInLeft');
+        } else {
+            item.classList.add('animate__fadeInRight');
+        }
+    });
+    
+    function animateArticles (index) {
+        if (index > 0) {
+            index = index * 2;
+            articleItems[index-2].style.display = 'flex';
+            articleItems[index-1].style.display = 'flex';
+        }
+    }
 });
 
     
